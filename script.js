654251fb9eb87b8887f0aca606d98b79c9c3a045
Unknown <AThompson027@west-mec.org>
2018-08-22 13:03:58 -0700

    Personal Instruction
    Filled in more code for personal instruction.

--- script.js
+++ script.js
@@ -1,77 +1,90 @@
  /*
-        Project 01_06_02
+          Project 01_06_02
 
-        Author: Annie Thompson
-        Date: 8-16-18
+          Author: Annie Thompson
+          Date: 8-16-18
 
-        Filename: script.js
-     */
-
-//*** DO NOT HAVE EXPLORER 8, CANNOT TEST***
+          Filename: script.js
+       */
 
  "use strict";
- // global variable
+
  var formValidity = true;
 
- // function to validate all the required fields
+ // function to validate required fields
  function validateRequired() {
-     var inputFields = document.querySelectorAll("#contactinfo input");
-     var errorMessage = document.getElementById("errorText");
+     // support variables for validation
+     var inputElements = document.querySelectorAll("#contactinfo input");
+     var errorDiv = document.getElementById("numErrorText");
      var fieldsetValidity = true;
-     var elementLength = inputFields.length;
+     var elementCount = inputElements.length;
      var currentElement;
 
+     // try catch to handle a loop through the fields
      try {
-         for (var i = 0; i < elementLength; i++) {
-             currentElement = inputFields[i];
-             // for the blank fields
+         // loop through input fields looking for blanks
+         for (var i = 0; i < elementCount; i++) {
+             currentElement = inputElements[i];
+             //blanks
              if (currentElement.value === "") {
                  currentElement.style.background = "rgb(255,233,233)";
                  fieldsetValidity = false;
              } else {
                  currentElement.style.background = "white";
-                 errorMessage.style.display = "none"
              }
-             }
-             if (fieldsetValidity === false) {
-                 throw "Please complete your personal info.";
-             }
-         
+         }
+
+         if (fieldsetValidity === false) {
+             throw "Please complete all Personal Information."
+         } else {
+             errorDiv.innerHTML = "none";
+             errorDiv.style.display = "";
+         }
      } catch (msg) {
-         errorMessage.style.display = "block";
-         errorMessage.innerHTML = msg;
-         formValidity = false;
+         errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        formValidity = false;
      }
  }
 
-// function to prevent the form from submitted on default
- function validateForm(submit) {
-     
-     if (submit.preventDefault) {
-         submit.preventDefault();
+
+ // function to validate form on submit event
+ function validateForm(evt) {
+     if (evt.preventDefault) {
+         evt.preventDefault();
      } else {
-         submit.returnValue = false;
+         evt.returnValue = false;
      }
      formValidity = true;
+
      validateRequired();
 
+     // results of validation tests
      if (formValidity === true) {
+         document.getElementById("errorText").innerHTML = "";
+         document.getElementById("errorText").style.display = "none";
          document.getElementsByTagName("form")[0].submit();
+     } else {
+         document.getElementById("errorText").innerHTML = "Please fix the indicated problems.";
+         document.getElementById("errorText").style.display = "block";
+         scroll(0, 0);
      }
  }
 
- // function to create event listeners for the form
+ // function to create our event listeners
  function createEventListeners() {
-    
-     var form = document.getElementsByTagName("form")[0];
-     if (form.addEventListener) {
-         form.addEventListener("submit", validateForm, false);
-     } else if (form.attachEvent) {
-         form.attachEvent("onsubmit", validateForm);
+     // event listener for submit
+     // event handeler validateForm() 
+     if (window.addEventListener) {
+         window.addEventListener("submit", validateForm, false);
+     } else if (window.attachEvent) {
+         window.attachEvent("onsubmit", validateForm);
      }
  }
 
- // event listeners for window when it loads
+ // add a load event listener
+ //event handler will be createEventListeners()
+
  if (window.addEventListener) {
      window.addEventListener("load", createEventListeners, false);
  } else if (window.attachEvent) {
